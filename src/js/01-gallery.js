f1fbd199812928be3b6ac3e05de35f2981fe8783
RomanPyryn <92987401+RomanPyryn@users.noreply.github.com>
2022-05-03 11:04:26 +0300

    Додає перше завдання

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,43 @@
 // Add imports above this line
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
 import { galleryItems } from './gallery-items';
 // Change code below this line
 
 console.log(galleryItems);
+
+const galleryEl = document.querySelector('.gallery');
+const galleryMarkup = createsGalleryItemsMarkup(galleryItems);
+
+galleryEl.insertAdjacentHTML('beforeend', galleryMarkup);
+
+function createsGalleryItemsMarkup(galleryItems) {
+    return galleryItems.map(({ preview, original, description }) => {
+        return `
+        <a class="gallery__item" href="${original}">
+            <img 
+                class="gallery__image" 
+                src="${preview}" 
+                alt="${description}" />
+        </a>
+        `
+    } ).join('');
+};
+
+galleryEl.addEventListener('click', onGalleryItemClick);
+
+function onGalleryItemClick(e) {
+    e.preventDefault();
+
+    if (e.target.classList.value !== 'gallery__image') {
+        return;
+    }
+
+    console.log(e.target.classList.value);
+};
+
+var lightbox = new SimpleLightbox('.gallery a', {
+    captions: true,
+    captionsData: "alt",
+    captionDelay: 250,
+});
