7387f24f80bf8be42027535450ee79935f679d8c
RomanPyryn <92987401+RomanPyryn@users.noreply.github.com>
2022-05-05 22:42:41 +0300

    Додає виправлення

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -11,25 +11,36 @@ const textareaEl = document.querySelector('.feedback-form textarea');
 formEl.addEventListener('submit', onFormSubmit);
 formEl.addEventListener('input', throttle(onFormInput, 500));
 
-function onFormInput(e) { 
+
+
+function onFormInput(e) {
+    formData.email = inputEl.value;
+    formData.message = textareaEl.value;
+
     formData[e.target.name] = e.target.value;
 
     localStorage.setItem(FEEDBACK, JSON.stringify(formData));
 };
 
-const savedData = JSON.parse(localStorage.getItem(FEEDBACK));
+
+    const savedData = JSON.parse(localStorage.getItem(FEEDBACK));
 
 if (savedData) {
-        savedData.email ? inputEl.value = savedData.email : inputEl.value = '';
-        savedData.message ? textareaEl.value = savedData.message : textareaEl.value = '';
+    // savedData.email ? inputEl.value = savedData.email : inputEl.value = '';
+    // savedData.message ? textareaEl.value = savedData.message : textareaEl.value = '';
+    inputEl.value = savedData.email
+    textareaEl.value = savedData.message
     };
+
+
 
 function onFormSubmit(e) { 
     e.preventDefault();
-    savedData ? console.log('Відпрацьовує savedData', savedData) : console.log('Відпрацьовує formData', formData);
+    console.log(formData);
     e.currentTarget.reset();
     localStorage.removeItem(FEEDBACK);
 };
 
 
+    
     
